9c146b15ebba675d829c7ebf9d3617471e71bc8f
Fabien Chereau <fabien.chereau@gmail.com>
2021-08-12 17:03:50 +0400

    Fix

--- smt-server/apache-frontend/src/index.php
+++ smt-server/apache-frontend/src/index.php
@@ -1,46 +1,47 @@
 <?php
 
-// This script shows in its most basic format a connection check
-// to CAS using SAML1.1
-
-// Load the CAS lib
 require_once(APPPATH.'/../vendor/autoload.php');
 $phpCAS = new phpCAS;
 
 // Enable debugging
-//phpCAS::setLogger();
+phpCAS::setLogger();
 // Enable verbose error messages. Disable in production!
-//phpCAS::setVerbose(true);
+phpCAS::setVerbose(true);
 
 // Initialize phpCAS
-phpCAS::client(SAML_VERSION_1_1, 'cas.cosmos.esa.int', 443, '/cas');
+phpCAS::client(CAS_VERSION_2_0, 'cas.cosmos.esa.int', 443, '/cas');
+
+// For production use set the CA certificate that is the issuer of the cert
+// on the CAS server and uncomment the line below
+// phpCAS::setCasServerCACert($cas_server_ca_cert_path);
 
 // For quick testing you can disable SSL validation of the CAS server.
-// Do not use this in production!
+// THIS SETTING IS NOT RECOMMENDED FOR PRODUCTION.
+// VALIDATING THE CAS SERVER IS CRUCIAL TO THE SECURITY OF THE CAS PROTOCOL!
 phpCAS::setNoCasServerValidation();
 
-// Force CAS authentication on any page that includes this file
-//phpCAS::forceAuthentication();
+// force CAS authentication
+phpCAS::forceAuthentication();
 
-?>
-<body>
+// at this step, the user has been authenticated by the CAS server
+// and the user's login name can be read with phpCAS::getUser().
 
-<h2>Advanced SAML 1.1 example</h2>
-Authentication succeeded for user
-<strong><?php echo phpCAS::getUser(); ?></strong>.
-
-<h3>User Attributes</h3>
-<ul>
-<?php
-foreach (phpCAS::getAttributes() as $key => $value) {
-if (is_array($value)) {
-echo '<li>', $key, ':<ol>';
-foreach ($value as $item)
-
-{ echo '<li><strong>', $item, '</strong></li>'; }
-echo '</ol></li>';
-} else
-
-{ echo '<li>', $key, ': <strong>', $value, '</strong></li>' . PHP_EOL; }
+// logout if desired
+if (isset($_REQUEST['logout'])) {
+  phpCAS::logout();
 }
+
+// for this test, simply print that the authentication was successfull
 ?>
+<html>
+  <head>
+    <title>phpCAS simple client</title>
+  </head>
+  <body>
+    <h1>Successfull Authentication!</h1>
+    <?php require 'script_info.php' ?>
+    <p>the user's login is <b><?php echo phpCAS::getUser(); ?></b>.</p>
+    <p>phpCAS version is <b><?php echo phpCAS::getVersion(); ?></b>.</p>
+    <p><a href="?logout=">Logout</a></p>
+  </body>
+</html>
